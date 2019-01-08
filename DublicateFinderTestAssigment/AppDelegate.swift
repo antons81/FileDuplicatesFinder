d7d694a70a031793a8c90e0e58e152c6abdacfc8
Anton Stremovskiy <perlik@gmail.com>
2019-01-08 23:43:19 +0200

    Initial Commit

---
+++ DublicateFinderTestAssigment/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DublicateFinderTestAssigment
+//
+//  Created by Anton Stremovskiy on 1/8/19.
+//  Copyright © 2019 áSoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DublicateFinderTestAssigment/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DublicateFinderTestAssigment
+//
+//  Created by Anton Stremovskiy on 1/8/19.
+//  Copyright © 2019 áSoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
